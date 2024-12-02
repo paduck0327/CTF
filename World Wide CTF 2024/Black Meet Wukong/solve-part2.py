e7f25697fecb1be63bcd42ab0555b9f37899c982
paduck0327 <108603385+paduck0327@users.noreply.github.com>
2024-12-02 22:23:35 +0900

    Create solve-part2.py

---
+++ World Wide CTF 2024/Black Meet Wukong/solve-part2.py
@@ -0,0 +1,6 @@
+import base64
+
+# https://github.com/QmxhY2sgTWVldCBXdWtvbmc/gnoku-_t-m_kcalb/blob/main/blackmeetwukong.py#L460
+encoded_flag = "6?IDoFD,B0+DGm>FD,5.CghC,+E1b0F?1O(0f26j?VOZ^87,Fu9/'+!I/"
+
+print(base64.a85decode(encoded_flag))
